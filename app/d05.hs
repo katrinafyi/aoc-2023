b13718b097ba269e56db532c2548597a5c435dfd
rina <k@rina.fyi>
2023-12-05 23:57:34 +1000

    intervalImage.

--- app/d05.hs
+++ app/d05.hs
@@ -14,7 +14,7 @@ import Data.Function
 import Data.Functor
 import Data.List
 import Data.Map (Map, (!))
-import qualified Data.Map as Map
+import qualified Data.Map.Strict as Map
 import Data.Maybe
 import Data.Sequence (Seq ((:<|)), (<|), (><), (|>))
 import qualified Data.Sequence as Seq
@@ -36,13 +36,21 @@ data Data = Data { seeds :: [Integer], maps :: Map T.Text (T.Text, Map Integer I
 -- lookups are performed by rounding down.
 intervalLookup x m = dest + x - src
   where
-    (src,dest) = fromMaybe (error "interval not in map") $ Map.lookupLE x m
+    (src,dest) = fromMaybe (0,0) $ Map.lookupLE x m
 
 intervalUpdate k (low, len) m
   | src0 == dest0 && src0 <= k = Map.insert k low m `Map.union` Map.singleton (k+len) (k+len)
   | otherwise = error "intervals overlap!"
   where
     (src0,dest0) = fromMaybe (error "interval not in map") $ Map.lookupLE (k + len - 1) m
+
+intervalImage m (k,len) = front ++ intervals
+  where
+    v = intervalLookup k m
+    between = Map.insert k v $ fst $ Map.split (k+len) $ snd $ Map.split k m
+    intervals = fmap (\((k1,v1),(k2,_)) -> (v1, k2-k1)) $ sliding2 $ Map.toList between
+    len' = sum $ fmap snd intervals
+    front = guard (len' < len) $> (snd (Map.findMax between), len - len')
 
 parse raw =
   -- Map.fromList maps
@@ -57,30 +65,26 @@ parsep = do
   fns <- flip P.sepBy (P.char '\n') $ do
     [low2, low1, len] <- uinteger `P.sepBy` P.char ' '
     pure $ intervalUpdate low1 (low2, len)
-    -- pure $ [low2, low1, len]
-  pure (from, (to, foldr' (.) id fns $ Map.singleton 0 0))
-  -- pure $ (from, (to, fns))
+  pure (from, (to, foldr' ($) (Map.singleton 0 0) fns))
 
-seedLocation (Data seeds maps) num = fromLeft undefined $ fixM (uncurry go) ("seed", num)
+seedLocation (Data seeds maps) interval = fromLeft undefined $ fixM (uncurry go) ("seed", [interval])
   where
-    go "location" num = Left num
-    go kind num = Right $ Data.Bifunctor.second (intervalLookup num) $ maps ! kind
+    go "location" is = Left is
+    go kind is = Right (fst m, concatMap (intervalImage (snd m)) is)
+      where m = maps ! kind
 
--- one :: Data -> Integer
-one d = minimum $ fmap (seedLocation d) (seeds d)
-  -- mapKeyed (flip intervalLookup $ snd $ maps ! "fertilizer") [0..55]
+one d = minimum $ concatMap (seedLocation d . interval) (seeds d)
+  where interval x = (x,1)
 
-two d = minimum $ fmap (seedLocation d) seeds'
+two d = minimum $ concatMap (seedLocation d) seeds'
   where
-    seeds' = do
-      [low, len] <- chunks 2 $ seeds d
-      [low..low+len+1]
-    -- goSeed num = rights $ takeWhile isRight $ iterateM (uncurry go) ("seed", num)
-
+    seeds' = fmap (\[x,y] -> (x,y)) $ chunks 2 $ seeds d
 
 main :: (HasCallStack) => IO ()
 main = do
   inp <- parse <$> getContents
   print $ inp
+  -- print $ intervalImage (snd $ maps inp ! "seed") (98, 4)
+  -- print $ flip intervalLookup (snd $ maps inp ! "seed") <$> [98 .. (98 + 3)]
   print $ one inp
   print $ two inp
