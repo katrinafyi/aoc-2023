d47667db4470968344d223d6ef82ad536600f03f
rina <k@rina.fyi>
2023-12-09 16:41:24 +1100

    d09.

--- app/d09.hs
+++ app/d09.hs
@@ -1,1 +1,52 @@
-main = putStrLn "d09"
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TupleSections #-}
+{-# LANGUAGE TypeApplications #-}
+
+import AocLib
+import Control.Applicative
+import Control.Arrow
+import Control.Monad
+import Control.Monad.Trans.State.Strict
+import Data.Bifunctor
+import Data.Char
+import Data.Either
+import Data.Foldable
+import Data.Function
+import Data.Functor
+import Data.List
+import Data.Map (Map, (!))
+import qualified Data.Map.Strict as Map
+import Data.Maybe
+import Data.Sequence (Seq ((:<|)), (<|), (><), (|>))
+import qualified Data.Sequence as Seq
+import Data.Set (Set)
+import qualified Data.Set as Set
+import qualified Data.Text as T
+import Data.Tuple ()
+import Debug.Trace
+import GHC.Stack (HasCallStack)
+import Text.ParserCombinators.ReadP
+import qualified Text.ParserCombinators.ReadP as P
+import Data.Ord
+
+parse = fmap ints . lines
+
+deltas = reverse . unfoldr go
+  where
+    go xs
+      | all (== 0) xs = Nothing
+      | otherwise = Just (xs, zipWith (-) (tail xs) xs)
+
+extrapolate f = foldl' f 0
+
+solve f get = extrapolate f . fmap get . deltas
+
+one = sum . fmap (solve (+) last)
+two = sum . fmap (solve (flip (-)) head)
+
+main :: HasCallStack => IO ()
+main = do
+  inp <- parse <$> getContents
+  print $ inp
+  print $ one inp
+  print $ two inp
